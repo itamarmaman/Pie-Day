4f17042f768ffd031af16533b6d8fd7436b2def4
itamarmaman <itamar.maman@gmail.com>
2020-03-07 00:40:29 +0200

    Updates

---
+++ precache-manifest.3f2558e48f1962f69f514064d9c4bd1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7635a3e3a55dcedef56274f7d5f9b9",
+    "url": "/Pie-Day/index.html"
+  },
+  {
+    "revision": "7441cc33db8345ec4d1f",
+    "url": "/Pie-Day/static/css/2.7fa60bb6.chunk.css"
+  },
+  {
+    "revision": "5e585272625777175749",
+    "url": "/Pie-Day/static/css/main.75e6c70a.chunk.css"
+  },
+  {
+    "revision": "7441cc33db8345ec4d1f",
+    "url": "/Pie-Day/static/js/2.623bd87c.chunk.js"
+  },
+  {
+    "revision": "5e585272625777175749",
+    "url": "/Pie-Day/static/js/main.defcc86d.chunk.js"
+  },
+  {
+    "revision": "08988b4b4c0172fda1cf",
+    "url": "/Pie-Day/static/js/runtime-main.8146e374.js"
+  },
+  {
+    "revision": "d85207d141f4291841af596ab502e524",
+    "url": "/Pie-Day/static/media/CameraSvg.d85207d1.svg"
+  },
+  {
+    "revision": "0e751996101dc9989249479588e1f1ee",
+    "url": "/Pie-Day/static/media/back.0e751996.svg"
+  }
+]);
